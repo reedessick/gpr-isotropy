0280e6b025ac17296769e15d39507a0e99a4e7b0
Reed Essick <reed.essick@ligo.org>
2018-08-09 00:03:32 -0500

    mucked around with exactly how likelihoods are instantiated and implemented Ro_Eps\.__call__

--- gpr_isotropy/likelihood.py
+++ gpr_isotropy/likelihood.py
@@ -13,11 +13,14 @@ class Likelihood(object):
     general class representing a likelihood
     is instantiated with references to data to avoid re-computing things
     """
-
     def __init__(self, maps, exposure):
-        ### set up the derived products we need from the maps
-        self._npix = len(maps[0])
+        self._exposure = exposure
+        self._npix = len(exposure)
         self._nside = hp.npix2nside(self._npix)
+
+        ### set up the derived products we need from the maps
+        self._maps = maps
+        self._num = len(maps)
         self._sum = np.zeros(self._npix, dtype=float)
         self._out = np.zeros((self._npix, self._npix), dtype=float)
         for m in maps:
@@ -25,18 +28,19 @@ class Likelihood(object):
             self._sum += m
             self._out += np.outer(m, m)
        
-        assert len(exposure)==self._npix, 'inconsistent shape for exposure!'
-        self._exposure = exposure
-
     def __call__(self, *args):
         return 0. ### flat likelihood
+
+#---
 
 class Ro_Eps(Likelihood):
     """
     likelihood for (Ro, eps) given maps, exposure
     """
-    def __call__(self, Ro, eps): 
-        raise NotImplementedError
+    def __call__(self, Ro, eps):
+        return -np.sum(Ro*self._exposure) + self._num*np.log(np.sum(Ro*self._exposure)) \
+            + np.sum(eps*Ro*(-self._exposure+self._sum)) \
+            -0.5*np.sum(eps*Ro*np.sum(self._out*Ro*eps, axis=1))
 
 class Ro(Likelihood):
     """
@@ -45,24 +49,16 @@ class Ro(Likelihood):
     analytic marginalization over eps
     """
     def __cal__(self, Ro, kernel):
-        raise NotImplementedError
+        raise NotImplementedError('')
 
-def Eps(Likelihood):
+def Eps(Ro_Eps):
     """
     log p(data|eps; maps, exposure, kernal)
 
     importance sampling to marginalize over Ro
     if Ro_samples is not supplied, we'll sample some from loglike_Ro
     """
-    def __init__(self, maps, exposure):
-        self._likelihood_Ro_eps = Ro_Eps(maps, exposure)
-        self._npix = self._likelihood_Ro_eps._npix
-        self._nside = self._likelihood_Ro_eps._nside
-        self._sum = self._likelihood_Ro_eps._sum
-        self._out = self._likelihood_Ro_eps._out
-        self._exposure = self._likelihood_Ro_eps._exposure
-
     def __call__(self, eps, Ro_samples):
-        ans = np.ans([self._likelihood_Ro_eps(Ro, eps) for Ro in Ro_samples])
+        ans = np.ans([Ro_Eps.__call__(self, Ro, eps) for Ro in Ro_samples])
         m = np.max(ans)
         return np.log(np.sum(np.exp(ans-m))) + m - np.log(len(Ro_samples))
