60422a97cab90263cb482501d94aaec747840bfd
Reed Essick <reed.essick@ligo.org>
2018-08-20 23:34:38 -0500

    added some more placeholder logic

--- gpr_isotropy/configparser.py
+++ gpr_isotropy/configparser.py
@@ -6,3 +6,19 @@ __author__ = "reed.essick@ligo.org"
 from ConfigParser import ConfigParser
 
 #-------------------------------------------------
+
+DEFAULT_NSIDE = 4
+
+#-------------------------------------------------
+
+def parse(path, nside=DEFAULT_NSIDE, verbose=False):
+    """
+    parse out the prior, likelihood, and posterior parameters from path
+    return prior, likelihood, posterior
+    """
+    if verbose:
+        print('reading config: '+path)
+    config = ConfigParser()
+    config.read(path)
+
+    raise NotImplementedError
